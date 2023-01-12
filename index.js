04ce52cf82979c151819b9b878a1d1dbc073eaf1
ansaryshohan <ansarye2083@gmaail.com>
2023-01-12 15:44:34 +0600

    jwt token generated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
+const jwt= require('jsonwebtoken')
 
 const app=express();
 const port= process.env.PORT||5000;
@@ -13,7 +14,25 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const run=()=>{
+  const userDataBase= client.db('carSellerDB').collection('users')
 
+try{
+
+  app.put('/user/:email',async(req,res)=>{
+    const email=req.params.email;
+    const query= {email:email};
+    const user= req.body;
+    const options= {upsert: true};
+    const updateDoc= { $set:user}
+    const result= await userDataBase.updateOne(query, updateDoc, options)
+    
+    const jwtToken= jwt.sign(user,process.env.JWT_KEY,{expiresIn:"1d"})
+    res.send({message:true, data:result , jwtToken})
+})
+}
+catch(error){
+  console.log(error)
+}
 
 }
 run() .catch(error=>console.log(error))
